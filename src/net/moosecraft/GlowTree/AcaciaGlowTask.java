0fd3f853111e99e03d9b9fa8c71b2b840cdc562c
Tony Windebank <anthonywindebank@gmail.com>
2024-01-03 04:06:39 -0500

    Updated for 2024!

--- src/net/moosecraft/GlowTree/AcaciaGlowTask.java
+++ src/net/moosecraft/GlowTree/AcaciaGlowTask.java
@@ -1,15 +1,11 @@
 package net.moosecraft.GlowTree;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.CreatureSpawner;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.world.StructureGrowEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -36,37 +32,14 @@ public class AcaciaGlowTask extends BukkitRunnable {
     	if (tTop.getRelative(BlockFace.UP).getType().equals(Material.AIR)){
     		tTop = tTop.getRelative(BlockFace.DOWN);
     	}
-        if(replace.equals(Material.MOB_SPAWNER)) {
-            	
-        	Block tSeed = tBottom.getBlock().getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN);
-        	CreatureSpawner seedSpawner = (CreatureSpawner)tSeed.getState();
-    		EntityType mtype = seedSpawner.getSpawnedType();
-        	
-        	ArrayList<Block> blockStates = new ArrayList<Block>(Arrays.asList(
-        			tTop.getRelative(BlockFace.NORTH),
-        			tTop.getRelative(BlockFace.SOUTH),
-        			tTop.getRelative(BlockFace.EAST),
-        			tTop.getRelative(BlockFace.WEST)));
-        	
-        	for ( Block block : blockStates){
-        		block.setType(Material.MOB_SPAWNER);
-        		CreatureSpawner spawner = (CreatureSpawner)block.getState();
-        		spawner.setSpawnedType(mtype);
-        		spawner.update();
-        		
-        	}
 
-        }//if block mobspawner
-        
-        //If its not a mobspawner we dont need to set mobspawner type
-    	else {
-            
-    		tTop.getRelative(BlockFace.NORTH).setType(replace);
-            tTop.getRelative(BlockFace.SOUTH).setType(replace);
-            tTop.getRelative(BlockFace.EAST).setType(replace);
-            tTop.getRelative(BlockFace.WEST).setType(replace);
 
-        }//else normal replacement
+        tTop.getRelative(BlockFace.NORTH).setType(replace);
+        tTop.getRelative(BlockFace.SOUTH).setType(replace);
+        tTop.getRelative(BlockFace.EAST).setType(replace);
+        tTop.getRelative(BlockFace.WEST).setType(replace);
+
+
         
         //replace the seed material with dirt when we're done
         tBottom.getBlock().getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN).setType(Material.DIRT);
