ffb7e4cfaa2d97145692dc375cbe71939a605010
Tony Windebank <anthonywindebank@gmail.com>
2024-01-03 04:05:53 -0500

    Updated for 2024!

--- src/net/moosecraft/GlowTree/GlowTask.java
+++ src/net/moosecraft/GlowTree/GlowTask.java
@@ -1,15 +1,13 @@
 package net.moosecraft.GlowTree;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.CreatureSpawner;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.world.StructureGrowEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -17,6 +15,26 @@ public class GlowTask extends BukkitRunnable {
 	private final StructureGrowEvent event;
 	private final Material seed;
 	HashMap<Material, GlowTree.treeType> enabledTreeList;
+	
+	private Material[] trees = {
+			Material.ACACIA_LOG,
+			Material.BIRCH_LOG,
+			Material.CHERRY_LOG,
+			Material.DARK_OAK_LOG,
+			Material.JUNGLE_LOG,
+			Material.MANGROVE_LOG,
+			Material.OAK_LOG,
+			Material.SPRUCE_LOG,
+			Material.STRIPPED_ACACIA_LOG,
+			Material.STRIPPED_BIRCH_LOG,
+			Material.STRIPPED_CHERRY_LOG,
+			Material.STRIPPED_DARK_OAK_LOG,
+			Material.STRIPPED_JUNGLE_LOG,
+			Material.STRIPPED_MANGROVE_LOG,
+			Material.STRIPPED_OAK_LOG,
+			Material.STRIPPED_SPRUCE_LOG };
+    
+	private List<Material> treeList = Arrays.asList(trees);
 	
 	public GlowTask(StructureGrowEvent e, Material s){
 		this.event = e;
@@ -30,46 +48,21 @@ public class GlowTask extends BukkitRunnable {
 
         final Location tBottom = event.getLocation();
         Material replace = GlowTree.seedList.get(GlowTree.enabledTreeList.get(seed));
+        
         //find the start of the tree leaves
         Block tTop = GlowTree.getLeaf(tBottom.getBlock());
     	
         //go up one more if there's extra room
-        if(tTop.getRelative(BlockFace.UP).getType().equals(Material.LOG)){
+        if(treeList.contains(tTop.getRelative(BlockFace.UP).getType()) ){
     		
     		tTop = tTop.getRelative(BlockFace.UP);
         }
 
-        if(replace.equals(Material.MOB_SPAWNER)) {
-        	
-        	Block tSeed = tBottom.getBlock().getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN);
-        	CreatureSpawner seedSpawner = (CreatureSpawner)tSeed.getState();
-    		EntityType mtype = seedSpawner.getSpawnedType();
-        	
-        	ArrayList<Block> blockStates = new ArrayList<Block>(Arrays.asList(
-        			tTop.getRelative(BlockFace.NORTH),
-        			tTop.getRelative(BlockFace.SOUTH),
-        			tTop.getRelative(BlockFace.EAST),
-        			tTop.getRelative(BlockFace.WEST)));
-        	
-        	for ( Block block : blockStates){
-        		block.setType(Material.MOB_SPAWNER);
-        		CreatureSpawner spawner = (CreatureSpawner)block.getState();
-        		spawner.setSpawnedType(mtype);
-        		spawner.update();
-        		
-        	}
-
-        }//if block mobspawner
-        
-        //If its not a mobspawner we dont need to set mobspawner type
-    	else {
-            
-    		tTop.getRelative(BlockFace.NORTH).setType(replace);
-            tTop.getRelative(BlockFace.SOUTH).setType(replace);
-            tTop.getRelative(BlockFace.EAST).setType(replace);
-            tTop.getRelative(BlockFace.WEST).setType(replace);
-
-        }//else normal replacement
+          
+    	tTop.getRelative(BlockFace.NORTH).setType(replace);
+        tTop.getRelative(BlockFace.SOUTH).setType(replace);
+        tTop.getRelative(BlockFace.EAST).setType(replace);
+        tTop.getRelative(BlockFace.WEST).setType(replace);
         
         //replace the seed material with dirt when we're done
         tBottom.getBlock().getRelative(BlockFace.DOWN).getRelative(BlockFace.DOWN).setType(Material.DIRT);
